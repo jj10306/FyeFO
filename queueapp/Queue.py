e33eb424d08eb13b236ea7b296169187c8b66a1b
Damian Henry <33043860+dhenry15@users.noreply.github.com>
2019-01-11 14:31:18 -0500

    Upload Queue.py

---
+++ queueapp/Queue.py
@@ -0,0 +1,39 @@
+class Queue:
+    def __init__(self):
+        self.CAPACITY = 25
+        self.arr = [None] * self.CAPACITY
+        self.front = 0 ## don't really need 'front' variable. can use back - size
+        self.back = 0
+        self.size = 0
+    def enqueue(self,student):
+        if student == None:
+            return "Student cannot be None!"
+        if self.size == len(self.arr):
+            return "Queue is full :("
+        else:
+            self.arr[self.back] = student
+            self.back = (self.back + 1) % len(self.arr)
+            self.size += 1
+    def dequeue(self):
+        if self.size == 0:
+            return "Queue is empty!"
+        returned = self.arr[self.front]
+        self.arr[self.front] = None
+        self.front = (self.front + 1) % len(self.arr)
+        self.size -= 1
+        return returned
+    def peek(self):
+        if self.size == 0:
+            return "Queue is empty!"
+        return self.arr[self.front]
+    def size(self):
+        return self.size
+    def clear(self):
+        self.arr = [None] * self.CAPACITY
+        self.front = 0
+        self.back = 0
+        self.size = 0
+    def isEmpty(self):
+        return size == 0
+    def __repr__(self):
+        return str(self.arr[self.front:self.back])
