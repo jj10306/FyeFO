d49fbd40c450068e33e48ef88df1da6242aa0209
Jakob Johnson <johnsonjakob99@gmail.com>
2019-01-14 19:54:44 -0500

    Setup imports for table display

--- queueapp/routes.py
+++ queueapp/routes.py
@@ -1,15 +1,21 @@
 from flask import render_template, request
 from queueapp import app
 from queueapp.forms import Form
+from queapp.Queue import Queue
+from queueapp.table import StudentTable
+from queueapp.Student import Student
+
+daQueue = Queue() #this is the queue whose contents are being displayed
 
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     form = Form()
 
+
     if request.method == 'POST':
         name = request.form["student_name"]
         choice = request.form["reason"]
-        print(name)
+
 
     return render_template('index.html', form=form)
 
