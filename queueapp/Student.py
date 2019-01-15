180e7fc8bcbb8447d132400460d70753e0a65c6f
Damian William Henry <damian@lawn-128-61-71-41.lawn.gatech.edu>
2019-01-14 19:34:26 -0500

    Added 'reason' instance variable to Student class

--- queueapp/Student.py
+++ queueapp/Student.py
@@ -1,8 +1,9 @@
 from time import localtime
 class Student:
-    def __init__(self,name):
+    def __init__(self,name, reason):
         self.name = name.strip()
         self.time = localtime()
+        self.reason = reason
     def getName(self):
         return self.name
     def getExtendedTime(self):
@@ -10,5 +11,5 @@ class Student:
     def getTime(self):
         return (self.time.tm_hour, self.time.tm_min, self.time.tm_sec, self.time.tm_wday)
     def __repr__(self):
-        return "{}({}:{}:{})".format(self.name, self.getTime()[0], self.getTime()[1], self.getTime()[2])
+        return "{}:{}({}:{}:{})".format(self.name,self.reason, self.getTime()[0], self.getTime()[1], self.getTime()[2])
       
