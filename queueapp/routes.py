1ad7eb23eaf6113ce65650e8a0b8a76d58201626
Damian William Henry <damian@lawn-128-61-67-48.lawn.gatech.edu>
2019-01-26 17:17:22 -0500

    Implemented POST-REDIRECT-GET pattern to remedy page refresh auto-submit issue

--- queueapp/routes.py
+++ queueapp/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, flash
+from flask import render_template, redirect, url_for, request, flash
 from queueapp import app
 from queueapp.forms import Form
 from queueapp.Queue import Queue
@@ -10,6 +10,9 @@ daQueue = Queue() #this is the queue whose contents are being displayed
 @app.route("/", methods = ['GET', 'POST'])
 def index():
     form = Form()
+    table = StudentTable(daQueue.__repr__())
+    if request.method == "GET":
+        return render_template('index.html', form=form, table=table)
 
     if request.method == "POST":
         if request.form["button"] == "Submit":
@@ -17,16 +20,22 @@ def index():
             choice = request.form["reason"]
             if name != "" and choice != "Select":
                 daQueue.enqueue(Student(name,choice))
-    table = StudentTable(daQueue.__repr__())
-
-    return render_template('index.html', form=form, table=table)
-
+        return redirect(url_for("queueIndex"))
 
 @app.route("/ta", methods = ['GET', 'POST'])
 def taIndex():
     form = Form()
+    table = StudentTable(daQueue.__repr__())
+    if request.method == "GET":
+        return render_template('taIndex.html', form=form, table=table)
     if request.method == "POST":
         if request.form["removeButton"] == "Remove":
             daQueue.dequeue()
-    table = StudentTable(daQueue.__repr__())
-    return render_template('taIndex.html', form=form, table=table)
+        return redirect(url_for("queueIndex"))
+
+@app.route("/queue", methods = ['GET'])
+def queueIndex():
+    form = Form()
+    if request.method == "GET":
+        table = StudentTable(daQueue.__repr__())
+        return render_template('queue.html', form=form, table=table)
