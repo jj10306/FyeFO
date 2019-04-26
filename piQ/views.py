c44bf6c6d001b1c64893a855aa678d9718c0041c
Jakob Johnson <johnsonjakob99@gmail.com>
2019-04-26 13:42:04 -0400

    added TAs on duty and set up structure for front-end to be worked on

--- piQ/views.py
+++ piQ/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, jsonify
+from flask import render_template, request, jsonify, url_for
 from piQ import app
 from piQ.models import Queue
 from piQ.forms import Form
@@ -6,6 +6,8 @@ from piQ.table import UserTable
 from piQ.logic import get_user_info
 
 source = Queue()
+tas = list()
+active_ta = ""
 
 @app.route("/", methods = ["GET", "POST"])
 def index():
@@ -14,20 +16,21 @@ def index():
     #always display table, regardlesss of if change was made or not
     # table=UserTable(source.__repr__())
 
-#serves the current queue to the front-end for manipulation/display
+#serves the current queue and ta list to the front-end for manipulation/display
 @app.route("/queue")
 def serve():
-    return jsonify({"queue": source.__repr__()})
+    return jsonify({"queue": source.__repr__(), "tas": tas})
 
 #takes in a request and does the required logic
 def process_request(request):
     global source
+    global name
 
     if request.method == "POST":
         #different possible TA button presses
         if request.form.get("remove"):
             source.dequeue()
-            return render_template("ta.html")
+            return render_template("ta.html", name=name)
         elif request.form.get("clear"):
             source = Queue()
             return render_template("index.html")
@@ -40,7 +43,9 @@ def process_request(request):
             name = user_data["name"]
 
             if user_data["role"] == "Ta":
-                return render_template("ta.html")
+                if name not in tas:
+                    tas.append(name)
+                return render_template("ta.html", name=name)
             else:
                 if not source.isElement(name):
                     source.enqueue(name)
